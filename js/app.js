6040529bc9b8d583ec4caa158d52fa9ae4092967
rigoCaicedo <rigotalga@gmail.com>
2021-09-21 16:05:41 -0500

    Validacion inicial para campos vacios

--- js/app.js
+++ js/app.js
@@ -1,13 +1,26 @@
 //variables
 let btnEnviar=document.querySelector('#enviar');
+let email=document.querySelector('#email');
+let asunto=document.querySelector('#asunto');
+let mensaje=document.querySelector('#mensaje')
 
 eventListeners();
 function eventListeners(){
     document.addEventListener('DOMContentLoaded',iniciarApp);
+    email.addEventListener('blur',validarFormulario);
+    asunto.addEventListener('blur',validarFormulario);
+    mensaje.addEventListener('blur',validarFormulario);
 }
 //funciones
 function iniciarApp(){
-    console.log('Iniciando la app');
     btnEnviar.disabled=true;
     btnEnviar.classList.add('cursor-not-allowed','opacity-50');
+}
+
+function validarFormulario(e){
+    if(e.target.value.length > 0){
+        console.log('Hay algo');
+    }else{
+        e.target.classList.add('border','border-red-500');
+    }
 }
