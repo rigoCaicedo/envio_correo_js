8478f26e1ec261c4f7bdef2c9ac6b328315c4f14
rigoCaicedo <rigotalga@gmail.com>
2021-09-21 16:20:03 -0500

    Imprimir mensaje de alerta al dejar campos vacios

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ let btnEnviar=document.querySelector('#enviar');
 let email=document.querySelector('#email');
 let asunto=document.querySelector('#asunto');
 let mensaje=document.querySelector('#mensaje')
+let formulario=document.querySelector('#enviar-mail');
 
 eventListeners();
 function eventListeners(){
@@ -22,5 +23,16 @@ function validarFormulario(e){
         console.log('Hay algo');
     }else{
         e.target.classList.add('border','border-red-500');
+        mostrarError();
+    }
+}
+
+function mostrarError(){
+    const mensajeError=document.createElement('p');
+    mensajeError.textContent='Todos los campos son obligatorios';
+    mensajeError.classList.add('border','border-red-500','background-red-100','text-red-500','p-3','mt-5','text-center','error');
+    const errores=document.querySelectorAll('.error');
+    if(errores.length===0){
+        formulario.appendChild(mensajeError);
     }
 }
