5f0d6506d4d83ad3648d8587b046a6292fc5587b
rigoCaicedo <rigotalga@gmail.com>
2021-09-22 10:40:17 -0500

    Se valida campo de correo con expresion regular y se corrigen las validaciones de campos vacios mediante clases css

--- js/app.js
+++ js/app.js
@@ -1,45 +1,56 @@
 //variables
-let btnEnviar=document.querySelector('#enviar');
-let email=document.querySelector('#email');
-let asunto=document.querySelector('#asunto');
-let mensaje=document.querySelector('#mensaje')
-let formulario=document.querySelector('#enviar-mail');
+let btnEnviar = document.querySelector('#enviar');
+let email = document.querySelector('#email');
+let asunto = document.querySelector('#asunto');
+let mensaje = document.querySelector('#mensaje')
+let formulario = document.querySelector('#enviar-mail');
 
 eventListeners();
-function eventListeners(){
-    document.addEventListener('DOMContentLoaded',iniciarApp);
-    email.addEventListener('blur',validarFormulario);
-    asunto.addEventListener('blur',validarFormulario);
-    mensaje.addEventListener('blur',validarFormulario);
+function eventListeners() {
+    document.addEventListener('DOMContentLoaded', iniciarApp);
+    email.addEventListener('blur', validarFormulario);
+    asunto.addEventListener('blur', validarFormulario);
+    mensaje.addEventListener('blur', validarFormulario);
 }
 //funciones
-function iniciarApp(){
-    btnEnviar.disabled=true;
-    btnEnviar.classList.add('cursor-not-allowed','opacity-50');
+function iniciarApp() {
+    btnEnviar.disabled = true;
+    btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
 }
 
-function validarFormulario(e){
-    if(e.target.value.length > 0){
-        console.log('Hay algo');
-    }else{
-        e.target.classList.add('border','border-red-500');
+function validarFormulario(e) {
+    if (e.target.value.length > 0) {
+        //eliminar los anuncions de errores
+        const error = document.querySelector('p.error');
+        error.remove();
+        e.target.classList.remove('border', 'border-red-500');
+        e.target.classList.add('border', 'border-green-500');
+    } else {
+        e.target.classList.remove('border', 'border-green-500');
+        e.target.classList.add('border', 'border-red-500');
         mostrarError('Todos los campos son obligatorios');
     }
 
-    if(e.target.type==='email'){
-        const resultado=e.target.value.indexOf('@');
-        if(resultado<0){
+    if (e.target.type === 'email') {
+        const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (er.test(e.target.value)) {
+            e.target.classList.remove('border', 'border-red-500');
+            e.target.classList.add('border', 'border-green-500');
+            console.log('El email válido');
+        } else {
+            e.target.classList.remove('border', 'border-green-500');
+            e.target.classList.add('border', 'border-red-500');
             mostrarError('El email no es válido');
         }
     }
 }
 
-function mostrarError(mensaje){
-    const mensajeError=document.createElement('p');
-    mensajeError.textContent=mensaje;
-    mensajeError.classList.add('border','border-red-500','background-red-100','text-red-500','p-3','mt-5','text-center','error');
-    const errores=document.querySelectorAll('.error');
-    if(errores.length===0){
+function mostrarError(mensaje) {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = mensaje;
+    mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5', 'text-center', 'error');
+    const errores = document.querySelectorAll('.error');
+    if (errores.length === 0) {
         formulario.appendChild(mensajeError);
     }
 }
